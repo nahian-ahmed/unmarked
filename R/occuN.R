d918755e6f47dcf542638a3057e27800dbcde74e
Nahian Ahmed <nahian.shafin.ahmed@gmail.com>
2025-10-09 00:53:49 -0700

    ...

--- R/occuN.R
+++ R/occuN.R
@@ -1,3 +1,98 @@
+
+
+
+# # Load TMB, which is needed for the model fitting
+# library(TMB)
+
+# #' @export
+# setClass("unmarkedFrameOccuN",
+#          slots = c(w = "matrix",
+#                    cellCovs = "data.frame"),
+#          contains = "unmarkedFrameOccu")
+
+# # Define the new output object for your model fit
+# #' @export
+# setClass("unmarkedFitOccuN",
+#          contains = "unmarkedFitOccu")
+
+
+# #' @export
+# unmarkedFrameOccuN <- function(y, siteCovs = NULL, obsCovs = NULL,
+#                                cellCovs, w, mapInfo = NULL) {
+
+#     if(is.null(siteCovs)) {
+#         siteCovs <- data.frame(site = 1:nrow(y))
+#     }
+#     parentFrame <- unmarkedFrameOccu(y = y, siteCovs = siteCovs,
+#                                      obsCovs = obsCovs, mapInfo = mapInfo)
+#     umf <- new("unmarkedFrameOccuN", parentFrame,
+#                w = w,
+#                cellCovs = cellCovs)
+#     return(umf)
+# }
+
+
+# # Custom getDesign method
+# setMethod("getDesign", "unmarkedFrameOccuN",
+#     function(umf, formula, na.rm = TRUE) {
+#     M <- numSites(umf)
+#     J <- obsNum(umf)
+#     det_formula <- as.formula(formula[[2]])
+#     state_formula <- as.formula(paste("~", formula[3], sep=""))
+#     sc <- umf@siteCovs
+#     if(nrow(sc) > 0) sc <- sc[rep(1:M, each = J), , drop = FALSE]
+#     oc <- as.data.frame(lapply(umf@obsCovs, as.vector))
+#     det_data <- cbind(sc, oc)
+#     mf_det <- model.frame(det_formula, det_data, na.action = na.pass)
+#     V_design <- model.matrix(det_formula, mf_det)
+#     mf_state <- model.frame(state_formula, umf@cellCovs, na.action = na.pass)
+#     X_design <- model.matrix(state_formula, mf_state)
+#     y <- getY(umf)
+#     return(list(y = y, X = X_design, V = V_design))
+# })
+
+
+# #' @export
+# occuN <- function(formula, data,
+#                   starts, method = "BFGS", control = list(), se = TRUE) {
+
+#     if(!is(data, "unmarkedFrameOccuN")) {
+#         stop("Data is not an object of class unmarkedFrameOccuN.")
+#     }
+
+#     designMats <- getDesign(data, formula)
+    
+#     tmb_data <- list(model = "tmb_occuN",
+#                      y = designMats$y,
+#                      X = designMats$X,
+#                      V = designMats$V,
+#                      w = data@w)
+    
+#     n_alpha <- ncol(designMats$V)
+#     n_beta <- ncol(designMats$X)
+
+#     if(missing(starts)) {
+#         starts <- rep(0, n_alpha + n_beta)
+#     }
+#     tmb_params <- list(alpha = starts[1:n_alpha],
+#                        beta = starts[(n_alpha + 1):(n_alpha + n_beta)])
+
+#     # --- This is the final fix ---
+#     # Point TMB to the correct DLL that contains all the TMB models.
+#     obj <- TMB::MakeADFun(data = tmb_data,
+#                           parameters = tmb_params,
+#                           DLL = "unmarked_TMBExports", # Correct DLL name
+#                           silent = TRUE)
+
+#     opt <- nlminb(obj$par, obj$fn, obj$gr, control = control)
+
+#     cat("SUCCESS! Model has been fit with TMB.\n")
+#     print(opt)
+    
+# }
+
+
+
 # Load TMB, which is needed for the model fitting
 library(TMB)
 
@@ -77,27 +172,24 @@ occuN <- function(formula, data,
                           DLL = "unmarked_TMBExports", silent = TRUE)
 
     opt <- nlminb(obj$par, obj$fn, obj$gr, control = control)
-
-    # --- This is the new, professional output formatting ---
     
-    # 1. Calculate standard errors and confidence intervals
     sd_rep <- TMB::sdreport(obj)
     est_mat <- summary(sd_rep)
     
-    # 2. Create unmarkedEstimate objects for each submodel
+    # --- This section is now corrected ---
+    
+    # Use 'covMat' and remove the 'se' argument
     state_est <- unmarkedEstimate(name = "State", short.name = "lam",
                                   estimates = est_mat[1:n_beta, 1],
-                                  se = est_mat[1:n_beta, 2],
-                                  cov.mat = sd_rep$cov.fixed[1:n_beta, 1:n_beta],
+                                  covMat = sd_rep$cov.fixed[1:n_beta, 1:n_beta],
                                   invlink = "exp", invlinkGrad = "exp")
 
+    # Use 'covMat' and remove the 'se' argument
     det_est <- unmarkedEstimate(name = "Detection", short.name = "p",
                                 estimates = est_mat[(n_beta + 1):n_pars, 1],
-                                se = est_mat[(n_beta + 1):n_pars, 2],
-                                cov.mat = sd_rep$cov.fixed[(n_beta + 1):n_pars, (n_beta + 1):n_pars],
+                                covMat = sd_rep$cov.fixed[(n_beta + 1):n_pars, (n_beta + 1):n_pars],
                                 invlink = "logistic", invlinkGrad = "logistic.grad")
 
-    # 3. Assemble the final unmarkedFit object
     fit <- new("unmarkedFitOccuN",
                fitType = "occuN",
                call = match.call(),
@@ -112,4 +204,3 @@ occuN <- function(formula, data,
 
     return(fit)
 }
-
