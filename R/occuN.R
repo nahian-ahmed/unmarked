fe10a45aafe1a877900f06d33c4441a6f54bc662
Nahian Ahmed <nahian.shafin.ahmed@gmail.com>
2025-11-02 16:54:39 -0800

    ...

--- R/occuN.R
+++ R/occuN.R
@@ -125,13 +125,13 @@ occuN <- function(formula, data,
 
 
 
-    # FIX: 'state' (beta) is SECOND in the parameter list
+    # 'state' (beta) is SECOND in the parameter list
     state_est <- unmarkedEstimate(name = "State", short.name = "lam",
                                   estimates = est_mat[(n_alpha + 1):n_pars, 1],
                                   covMat = sd_rep$cov.fixed[(n_alpha + 1):n_pars, (n_alpha + 1):n_pars],
                                   invlink = "exp", invlinkGrad = "exp")
 
-    # FIX: 'det' (alpha) is FIRST in the parameter list
+    # 'det' (alpha) is FIRST in the parameter list
     det_est <- unmarkedEstimate(name = "Detection", short.name = "p",
                                 estimates = est_mat[1:n_alpha, 1],
                                 covMat = sd_rep$cov.fixed[1:n_alpha, 1:n_alpha],
